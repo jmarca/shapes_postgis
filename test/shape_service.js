9a541fa1b59fca7726d627957ac231de9a0323fd
James E. Marca <jmarca@translab.its.uci.edu>
2012-09-13 22:05:56 -0700

    added the ability to merge multiple queries
    pass a callback as the last argument, and you can chain queries
    See the last test in test/shape_service for an example
    
    modified:   lib/shape_geojson_generation.js
    modified:   package.json
    modified:   test/shape_service.js

--- test/shape_service.js
+++ test/shape_service.js
@@ -10,7 +10,7 @@ var sgg = require('../lib/shape_geojson_generation')
 var shape_service = sgg.shape_service;
 var http = require('http')
 var express = require('express')
-
+var async = require('async')
 
 var env = process.env;
 var puser = process.env.PSQL_USER ;
@@ -242,6 +242,121 @@ describe ('shape_service', function(){
                                       }
                                   });
                            _.keys(fipscheck).should.be.empty
+                           return done()
+                       })
+           })
+    })
+    describe('two points tables', function(){
+        var app,server;
+
+        before(
+            function(done){
+                app = express()
+
+                var vds_options={'db':'osm'
+                                ,'table':'newtbmap.tvd'
+                                ,'alias':'tvd'
+                                ,'host':phost
+                                ,'username':puser
+                                ,'password':ppass
+                                ,'select_properties':{'tvd.freeway_id' : 'freeway'
+                                                     ,'tvd.freeway_dir': 'direction'
+                                                     ,"'vdsid_' || id"   : 'detector_id'
+                                                     ,'vdstype'        : 'type'
+                                                     }
+                                ,'id_col':'detector_id'
+                                }
+                var wim_options={'db':'osm'
+                                ,'table':'osm_upgraded_2010.twim'
+                                ,'alias':'twim'
+                                ,'host':phost
+                                ,'username':puser
+                                ,'password':ppass
+                                ,'select_properties':{'twim.freeway_id' : 'freeway'
+                                                     ,'twim.direction': 'direction'
+                                                     ,"'wimid_' || site_no"   : 'detector_id'
+                                                     ,'wim_type'        : 'type'
+                                                     }
+                                ,'id_col':['detector_id','direction']
+                                }
+
+                // now chain the services with callbacks
+
+                var chained_service = function(req,res,next){
+                    async.parallel([function(cb){
+                                        shape_service(vds_options
+                                                     ,function(data,req,res,next){
+                                                          cb(null,data)
+                                                      })(req,res,next)
+                                    }
+                                   ,function(cb){
+                                        shape_service(wim_options
+                                                     ,function(data,req,res,next){
+                                                          cb(null,data)
+                                                      })(req,res,next)
+                                    }]
+                                  ,function(err,results){
+
+                                       var data = results[0]
+                                       var wim = results[1]
+                                       if(data && data.features === undefined){
+                                           data = wim
+                                       }else{
+                                           if(wim
+                                            && wim.features !== undefined
+                                            && wim.features.length){
+                                               data.features.push(wim.features)
+                                               data.features = _.flatten(data.features)
+                                           }
+                                       }
+                                       res.writeHead(200, { 'Content-Type': 'application/json' });
+                                       res.end(JSON.stringify(data));
+                                   })
+                    return null
+                }
+
+                app.get('/points/:zoom/:column/:row.:format'
+                       ,chained_service
+                       )
+                server=http
+                       .createServer(app)
+                       .listen(testport,done)
+
+            })
+        after(function(done){
+            server.close(done)
+        })
+
+        it('should produce vds points in a box'
+          ,function(done){
+               // load the service for vds shape data
+               request({url:'http://'+ testhost +':'+testport+'/points/14/2821/6558.json'
+                       ,'headers':{'accept':'application/json'}
+                       ,followRedirect:true}
+                      ,function(e,r,b){
+                           if(e) return done(e)
+                           r.statusCode.should.equal(200)
+                           should.exist(b)
+                           var c = JSON.parse(b)
+                           c.should.have.property('type','FeatureCollection')
+                           c.should.have.property('features')
+                           c.features.should.have.length(15)
+                           var vds_match=false
+                           var wim_match = false;
+                           var vds_regex = /vdsid_\d{6,7}/;
+                           var wim_regex = /wimid_\d+_[NSEW]/;
+                           _.each(c.features
+                                 ,function(member){
+                                      member.should.have.property('geometry')
+                                      member.should.have.property('properties')
+                                      member.properties.should.have.property('id')
+                                      if(vds_regex.test(member.properties.id))
+                                          vds_match = true
+                                      if(wim_regex.test(member.properties.id))
+                                          wim_match = true
+                                  });
+                           vds_match.should.be.true
+                           wim_match.should.be.true
                            return done()
                        })
            })
